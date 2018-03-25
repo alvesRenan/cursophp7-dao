01faad40b2f4c25e87bb1904d8553f2a0050432d
Renan Alves <barbosa.renan.alves@gmail.com>
2018-03-25 14:32:49 -0300

    Métodos de listagem adicionados

--- index.php
+++ index.php
@@ -8,10 +8,31 @@ require_once("config.php");
 
 // echo json_encode($usuarios);
 
+
+/*
+ * Carrega 1 usuario
+ */
+// $usuario = new Usuario();
+// $usuario->loadById(1);
+// echo $usuario;
+
+/*
+ * Carrega todos os usuarios
+ */
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+/*
+ * Carrega uma lista de usuarios pelo login
+ */
+// $search = Usuario::search("f");
+// echo json_encode($search);
+
+/*
+ * Carrega um usuario passando o login e a senha
+ */
 $usuario = new Usuario();
-
-$usuario->loadById(1);
-
+$usuario->login("Renan", "1234");
 echo $usuario;
 
  ?>
